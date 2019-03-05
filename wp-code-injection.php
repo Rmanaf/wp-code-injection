d658245cffb4ae068de0a70836324bb9f2ab16f0
Rmanaf <rman.afzal@gmail.com>
2019-03-05 17:40:50 +0330

    2.2.8.78

--- wp-code-injection.php
+++ wp-code-injection.php
@@ -77,6 +77,7 @@ require_once __DIR__ . '/includes/calendar-heatmap.php';
 require_once __DIR__ . '/includes/package-manager.php';
 require_once __DIR__ . '/includes/code-metabox.php';
 require_once __DIR__ . '/includes/ajax-call-handler.php';
+require_once __DIR__ . '/includes/code-type.php';
 
 
 
