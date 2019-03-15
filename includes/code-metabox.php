2b6d8429a75e2b801e29fb0ea46f3336ec77c432
Rmanaf <rman.afzal@gmail.com>
2019-03-15 16:44:07 +0330

    2.2.8.138

--- includes/code-metabox.php
+++ includes/code-metabox.php
@@ -144,11 +144,10 @@ if (!class_exists('WP_CI_Code_Metabox')) {
              */
             public static function get_code_options($code)
             {
-                if(is_numeric($code))
-                {
-                    $ID = $code;
-                }
-                else
+                
+                $ID = $code;
+
+                if(isset($code->ID))
                 {
                     $ID = $code->ID;
                 }
