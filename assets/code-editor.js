3e41a7f960e9aa2c2a876f96fed0f817f3452fd1
Rmanaf <rman.afzal@gmail.com>
2019-04-02 17:59:05 +0430

    2.3.1.33

--- assets/code-editor.js
+++ assets/code-editor.js
@@ -16,28 +16,26 @@
 
     function hide(el){
         el.forEach(element => {
-            document.querySelector(element).style.display = 'none!important';
+            document.querySelector(element).style.display = 'none';
         });
     }
 
     function init(){
 
-        console.log('init');
-
         var textarea = document.querySelector('.wp-editor-area');
-
         var parent = document.getElementById('postdivrich');
-
         var container = document.createElement('DIV');
 
         container.classList.add('dcp-ci-editor');
 
         parent.insertBefore(container, parent.firstChild);
 
-        hide(['.quicktags-toolbar','#wp-content-editor-tools','#post-status-info']);
+        hide(['.quicktags-toolbar','#wp-content-editor-tools','#post-status-info','.wp-editor-area']);
 
         require(['vs/editor/editor.main'], function() { 
             var editor = monaco.editor.create(container , {
+                value: textarea.innerHTML,
+                theme: 'vs-dark',
                 language: 'php'
             });
         });
