8d4fa259f6468dd22f961671df26fb1f1d868627
Rmanaf <rman.afzal@gmail.com>
2019-04-02 20:01:56 +0430

    2.3.1.60

--- assets/code-editor.js
+++ assets/code-editor.js
@@ -60,7 +60,7 @@
 
     function createElement(t, ...className) {
         var res = document.createElement(t);
-        addClass(res, className);
+        addClass(res, ...className);
         return res;
     }
 
