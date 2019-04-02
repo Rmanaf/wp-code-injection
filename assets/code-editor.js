c05959e9e39aeaf8777c51ac168b36daeea0b102
Rmanaf <rman.afzal@gmail.com>
2019-04-02 19:53:17 +0430

    2.3.1.56

--- assets/code-editor.js
+++ assets/code-editor.js
@@ -89,11 +89,13 @@
         }
     }
 
-    function addClass(el, className) {
+    function addClass(el, ...className) {
         if (el.classList) {
             el.classList.add(className);
         } else {
-            el.className += ' ' + className;
+            className.forEach(c => {
+                el.className += ' ' + c;
+            });
         }
     }
 
