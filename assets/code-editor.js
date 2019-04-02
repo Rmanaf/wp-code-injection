9eb0b6017237c5c4641bac197694064828acf548
Rmanaf <rman.afzal@gmail.com>
2019-04-02 19:26:47 +0430

    2.3.1.48

--- assets/code-editor.js
+++ assets/code-editor.js
@@ -1,59 +1,99 @@
-;(() => {
+; (() => {
     "user strict"
 
     var editor = null;
+    var parent = null;
 
     function ready(fn) {
-        if (document.readyState != 'loading'){
-          fn();
+        if (document.readyState != 'loading') {
+            fn();
         } else if (document.addEventListener) {
-          document.addEventListener('DOMContentLoaded', fn);
+            document.addEventListener('DOMContentLoaded', fn);
         } else {
-          document.attachEvent('onreadystatechange', function() {
-            if (document.readyState != 'loading')
-              fn();
-          });
+            document.attachEvent('onreadystatechange', function () {
+                if (document.readyState != 'loading')
+                    fn();
+            });
         }
     }
 
-    function hide(el){
+    function hide(el) {
         el.forEach(element => {
             document.querySelector(element).style.display = 'none';
         });
     }
 
-    function init(){
+    function init() {
+
+        parent = document.getElementById('postdivrich');
 
         var textarea = document.querySelector('.wp-editor-area');
-        var parent = document.getElementById('postdivrich');
-        var toolbar =  document.createElement('DIV');
-        var container = document.createElement('DIV');
 
-        toolbar.classList.add('dcp-ci-toolbar');
-        container.classList.add('dcp-ci-editor');
-        parent.classList.add('fullscreen');
+        var toolbar = document.createElement('DIV');
+        var container = document.createElement('DIV');
+        var fullscreen = document.createElement('DIV');
+
+        addClass(toolbar , 'dcp-ci-toolbar');
+        addClass(container ,'dcp-ci-editor' );
+        addClass(fullscreen , 'full-screen');
 
         parent.insertBefore(container, parent.firstChild);
-        parent.insertBefore(toolbar , parent.firstChild);
+        parent.insertBefore(toolbar, parent.firstChild);
+        toolbar.appendChild(fullscreen);
 
-        hide(['.quicktags-toolbar','#wp-content-editor-tools','#post-status-info','.wp-editor-area', '#wp-content-wrap']);
+        hide(['.quicktags-toolbar', '#wp-content-editor-tools', '#post-status-info', '.wp-editor-area', '#wp-content-wrap']);
 
-        require(['vs/editor/editor.main'], () => { 
-            editor = monaco.editor.create(container , {
+        require(['vs/editor/editor.main'], () => {
+            editor = monaco.editor.create(container, {
                 value: textarea.textContent,
                 theme: 'vs-dark',
                 language: 'php'
             });
         });
 
+        toolbar.onclick = toggleFullScreen;
+
+        window.onresize = () => {
+            if (editor) {
+                editor.layout();
+            }
+        }
+
+    }
+
+    function toggleFullScreen() {
+        if(hasClass(parent , 'fullscreen')){
+            removeClass(parent , 'fullscreen');
+        }else{
+            addClass(parent , 'fullscreen');
+        }
+    }
+
+    function hasClass(el, className) {
+        if (el.classList) {
+            el.classList.contains(className);
+        } else {
+            new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+        }
+    }
+
+    function removeClass(el, className) {
+        if (el.classList) {
+            el.classList.remove(className);
+        } else {
+            el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+    }
+
+    function addClass(el, className) {
+        if (el.classList) {
+            el.classList.add(className);
+        } else {
+            el.className += ' ' + className;
+        }
     }
 
     ready(init);
 
-    window.onresize = () => {
-        if(editor){
-            editor.layout();
-        }
-    }
 
 })();
