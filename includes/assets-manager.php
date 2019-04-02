20f6a20f9116a79839f6738a11b416029f9c4960
Rmanaf <rman.afzal@gmail.com>
2019-04-02 15:58:20 +0430

    2.3.1.11

--- includes/assets-manager.php
+++ includes/assets-manager.php
@@ -101,7 +101,10 @@ if (!class_exists('WP_CI_Assets_Manager'))
             $ver = $this->version;
 
             // monaco-editor
+            wp_register_script('dcp-monaco-editor-loader', plugins_url('assets/monaco-editor/vs/loader.js', $this->plugin), [], $ver, false);
+            wp_register_script('dcp-monaco-editor-nls', plugins_url('assets/monaco-editor/vs/editor/editor.main.nls.js', $this->plugin), [], $ver, false);
             wp_register_script('dcp-monaco-editor', plugins_url('assets/monaco-editor/vs/editor/editor.main.js', $this->plugin), [], $ver, false);
+            
 
             // tagEditor
             wp_register_script('dcp-caret', plugins_url('assets/jquery.caret.min.js', $this->plugin), ['jquery'], $ver, false);
